208ac018418ce41e32a192c6e1a919f7fc90ea57
Oscar Ramo <oscar.ramo1289@gmail.com>
2017-02-23 18:35:58 -0600

    quinto intento y parece el bueno

---
+++ server/config/config.js
@@ -0,0 +1,10 @@
+// $env:NODE_ENV="development"
+var env = process.env.NODE_ENV || 'development';
+console.log('env ****', env);
+if (env === 'development') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoList';
+}else if (env === 'test') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoListTest';
+}
